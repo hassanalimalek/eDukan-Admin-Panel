1a8b1b8da6df7b0ebe4734d9adecb764b1bb0975
Hassan Ali <hassanalimalek9@gmail.com>
2021-01-28 19:13:47 +0500

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,11 @@
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDI10h-qzV2U89Gcj3HylZ3TU1c40xhkHo",
+    authDomain: "edukan-856f6.firebaseapp.com",
+    databaseURL: "https://edukan-856f6-default-rtdb.firebaseio.com",
+    projectId: "edukan-856f6",
+    storageBucket: "edukan-856f6.appspot.com",
+    messagingSenderId: "196505660455",
+    appId: "1:196505660455:web:76b1514a76e10f4f423202",
+    measurementId: "G-BZBX9EN17Z"
+  };
